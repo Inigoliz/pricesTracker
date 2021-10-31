790d5422519af842d4e818983f8625aae5a1002f
Inigoliz <inigoliz@gmail.com>
2021-10-31 15:08:40 +0100

    shell to be called from crontab

---
+++ runInVenv.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Script to initiate the Virtual Environment and run the code within
+
+source ~/Documents/Venvironments/sandbox/bin/activate
+
+#python3 ~/Documents/Proyects/pricesTracker/sandbbox.py
+python3 ~/Documents/Proyects/pricesTracker/pricesCollector.py
