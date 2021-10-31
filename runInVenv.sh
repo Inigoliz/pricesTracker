6b6967c1a6b545ea9857d245810fba6845972a27
Inigoliz <inigoliz@gmail.com>
2021-10-31 17:16:04 +0100

    Share over github added

--- runInVenv.sh
+++ runInVenv.sh
@@ -4,4 +4,9 @@
 source ~/Documents/Venvironments/sandbox/bin/activate
 
 #python3 ~/Documents/Proyects/pricesTracker/sandbbox.py
-python3 ~/Documents/Proyects/pricesTracker/pricesCollector.py
+#python3 ~/Documents/Proyects/pricesTracker/pricesCollector.py
+
+# Share ffiles over github
+cd /home/pi/Documents/Proyects/pricesTracker/
+git add /home/pi/Documents/Proyects/pricesTracker/data/*
+git push
