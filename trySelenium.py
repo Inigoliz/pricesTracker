7d14954ed820c044cf1e64e2a64edf1b09741276
Inigoliz <inigoliz@gmail.com>
2021-10-31 11:29:18 +0100

    added support for RPi

--- trySelenium.py
+++ trySelenium.py
@@ -7,15 +7,20 @@ from selenium.webdriver.common.by import By
 import time
 import random
 from datetime import datetime
+import os
 
 import numpy as np
 import pandas as pd
 
+path = str(os.path.dirname(os.path.abspath(__file__)))
 
-service = Service('/Users/tesla/Mis Cosas/Proyectos/ryanairPricesTracker/chromedriver')
+#service = Service('/Users/tesla/Mis Cosas/Proyectos/ryanairPricesTracker/chromedriver')
+service = Service('/usr/lib/chromium-browser/chromedriver') # RPi
+
 options = Options()
 options.headless = True
 options.add_argument("--window-size=1920,1200")
+
 
 ryanair = "https://www.ryanair.com/es/es/trip/flights/select?adults=1&teens=0&children=0&infants=0&dateOut=2022-01-15&dateIn=&isConnectedFlight=false&isReturn=false&discount=0&promoCode=&originIata=CPH&destinationIata=MAD&tpAdults=1&tpTeens=0&tpChildren=0&tpInfants=0&tpStartDate=2022-01-15&tpEndDate=&tpDiscount=20&tpPromoCode=&tpOriginIata=CPH&tpDestinationIata=MAD"
 
