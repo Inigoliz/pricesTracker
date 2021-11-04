03b8e09a1395f0795f39c02af23b1b82899e12d6
Inigoliz <inigoliz@gmail.com>
2021-11-04 23:47:36 +0100

    shell x2

---
+++ runInVenv2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Script to initiate the Virtual Environment and run the code 
+
+source ~/Documents/Venvironments/sandbox/bin/activate
+
+# Gather prices
+python3 ~/Documents/Proyects/pricesTracker/pricesCollector-MAD-CPH.py
+
+# Share files over github
+cd /home/pi/Documents/Proyects/pricesTracker/
+git add /home/pi/Documents/Proyects/pricesTracker/data_MAD-CPH/* 
+git commit -m "new datafile"
+git push
